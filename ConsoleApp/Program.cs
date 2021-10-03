a50916c166a4c6340b7bd74a63364ebdee693521
Thud Bloodseeker <antibody13@hotmail.com>
2021-10-03 13:28:05 +0700

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
